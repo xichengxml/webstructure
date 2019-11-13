fbfe637e24e224e48a842d6b3e0df4c9dd2ebe22
xichengxml <xichengxml@163.com>
2019-11-14 00:02:04 +0800

    fix

--- src/main/java/com/xicheng/chapter04_1/proxy/dynamic/jdk/HelloMain.java
+++ src/main/java/com/xicheng/chapter04_1/proxy/dynamic/jdk/HelloMain.java
@@ -1,5 +1,8 @@
 package com.xicheng.chapter04_1.proxy.dynamic.jdk;
 
+import com.xicheng.chapter04_1.proxy.common.Hello;
+import com.xicheng.chapter04_1.proxy.common.HelloImpl;
+
 public class HelloMain {
 
     public static void main(String[] args) {
